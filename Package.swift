5e9ca7d3c53bca0d3e9d19a4a122c396a196cd79
KivySwiftPackages <riper-chocks0g@icloud.com>
2023-10-12 19:09:20 +0200

    init upload

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "PyFoundation",
+	platforms: [.iOS(.v13)],
+	products: [
+		.library(name: "PyFoundation", targets: ["PyFoundation"])
+	],
+	dependencies: [
+        .package(url: "https://github.com/PythonSwiftLink/KivySwiftLink", branch: "master"),
+        .package(url: "https://github.com/PythonSwiftLink/SwiftonizePlugin", from: .init(0, 0, 0))
+	],
+	targets: [
+		.target(
+			name: "PyFoundation",
+			dependencies: [
+                .product(name: "PythonSwiftCore", package: "KivySwiftLink"),
+                .product(name: "PySwiftObject", package: "KivySwiftLink"),
+				//"SwiftOpenCV",
+
+			],
+			plugins: [ .plugin(name: "Swiftonize", package: "SwiftonizePlugin") ]
+		),
+
+	]
+)
