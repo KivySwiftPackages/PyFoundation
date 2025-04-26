8d13377fe6a0ade904cda981799c7ddcbf27c128
PsycHoWasP <psychowaspx@gmail.com>
2025-04-27 01:08:24 +0200

    Update PyFoundation.swift

--- Sources/PyFoundation/PyFoundation.swift
+++ Sources/PyFoundation/PyFoundation.swift
@@ -1,1 +1,10 @@
 
+import UIKit
+import PySerializing
+import PythonCore
+
+extension NSItemProvider: PySerializing.PySerialize {
+    public var pyPointer: PyPointer {
+        Self.asPyPointer(self)
+    }
+}
